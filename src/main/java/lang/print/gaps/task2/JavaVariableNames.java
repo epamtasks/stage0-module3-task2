6cc6217d0a6e280ba4f9fff39858fc876ff7d9d0
dilyor91 <dilyor_05@mail.ru>
2022-07-26 12:26:55 +0500

    Task #2. Key Points ver5

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int _morrow = 0;
-        int _money = 10;
-        int _hundred = 100;
-        int _back = 1000;
+        int  morrow = 0;
+        int money = 10;
+        int hundred = 100;
+        int back = 1000;
         int _underscore = 10000;
-        int _sharp = 100000;
-        int $star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int sharp = 100000;
+        int star = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
 
